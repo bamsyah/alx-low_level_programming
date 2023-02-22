48414a1d824ed6e728dc3c2280911d91976649bf
Msyah <bmsyah2000@gmail.com>
2023-02-22 08:09:12 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	i = 0;
+	while (c[i])
+	{
+		_putchar(p[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
