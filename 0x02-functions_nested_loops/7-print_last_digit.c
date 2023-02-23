6f2af3de281782cfd61ce1aa0315d0be4a8f6ae1
Msyah <bmsyah2000@gmail.com>
2023-02-22 17:05:56 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_last_digit - print last number;
+ *
+ * @n: number give them in the input
+ *
+ * Return: Alwas return positive number
+ */
+int print_last_digit(int n)
+{
+	if (n == -2147483648)
+	{
+		_putchar(8 + 48);
+		return (8);
+	}
+	else if (n < 0)
+	{
+		n *= -1;
+		_putchar((n % 10) + 48);
+		return (n % 10);
+	}
+	else
+	{
+		_putchar((n % 10) + 48);
+		return (n % 10);
+	}
+}
