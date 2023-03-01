613d5c62d1750cdc77b449f20dc7195d1cf15709
Msyah <bmsyah2000@gmail.com>
2023-02-28 18:01:51 -0800

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+
+
+#include "main.h"
+/**
+ * puts2 - print random char
+ *
+ * @str: input
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+	int j;
+
+	j = 0;
+	while (str[j])
+		j++;
+	i = 0;
+	while (i < j)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
