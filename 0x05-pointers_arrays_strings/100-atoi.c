1e983589d5c0d2e859f5cf9bbe51561540e63a3e
Msyah <bmsyah2000@gmail.com>
2023-02-28 18:05:43 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,48 @@
+
+
+/**
+ * _atoi - print convert a number
+ *
+ * @s: input
+ *
+ * Return: return number type int
+ */
+int _atoi(char *s)
+{
+	int i;
+	int res;
+	int sign;
+	int lenght;
+
+	lenght = 0;
+	res = 0;
+	i = 0;
+	sign = 1;
+	if (s[0] == '\0')
+		return (res);
+	while (s[lenght])
+		lenght++;
+	while (s[i] < '0' || s[i] > '9')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] == '\n')
+			return (0);
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (sign == 1)
+		{
+			res = res * 10;
+			res += s[i] - 48;
+		}
+		else if (sign == -1)
+		{
+			res = res * 10;
+			res += -s[i] + 48;
+		}
+		i++;
+	}
+	return (res);
+}
