f299ee2362a8b110c70a5ca8a4f4adbeae457797
Msyah <bmsyah2000@gmail.com>
2023-03-02 05:56:12 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+
+
+/**
+ * _strncat - it's like strcat but the defernt in n
+ *
+ * @dest: first input
+ *
+ * @src: second input
+ *
+ * @n: third input
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+	int lenght;
+
+	lenght = 0;
+	i = 0;
+	j = 0;
+	while (src[lenght])
+		lenght++;
+	while (dest[j])
+		j++;
+	if (lenght < n)
+		n = lenght;
+	while (i < n)
+	{
+		dest[j] = src[i];
+		j++;
+		i++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
